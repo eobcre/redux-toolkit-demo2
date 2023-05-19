25e8734b17deffa4b36f7df55e2b58681cfcaa16
eobcre <eobcre@gmail.com>
2023-05-19 11:52:57 -0400

    use of state and actions logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { add, minus, addByAmount } from './redux/countSlice';
+
 const App = () => {
-  return <div></div>;
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(add())}>Increase</button>
+      <button onClick={() => dispatch(minus())}>Decrease</button>
+      <button onClick={() => dispatch(addByAmount(10))}>Increase by 10</button>
+    </>
+  );
 };
 
 export default App;
